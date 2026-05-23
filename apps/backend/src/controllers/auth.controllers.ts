cfac1eeb74904d355aa97df89f5295a53ec6ba51
0xDev05 <divpatel1274@gmail.com>
2026-05-24 01:21:20 +0530

    feat: implement sign in endpoint

--- apps/backend/src/controllers/auth.controllers.ts
+++ apps/backend/src/controllers/auth.controllers.ts
@@ -3,7 +3,8 @@ import { authSchema } from "types/auth";
 import { sendValidationError } from "../utils/validation";
 import { prisma } from "db";
 import bcrypt from "bcrypt";
-
+import jwt from "jsonwebtoken";
+import { env } from "../utils/env";
 
 export const signUp = async (req: Request, res: Response): Promise<void>  => {
     const parsedBody = authSchema.safeParse(req.body);
@@ -41,5 +42,43 @@ export const signUp = async (req: Request, res: Response): Promise<void>  => {
 }
 
 export const singIn = async (req: Request, res: Response) => {
+    const parsedBody = authSchema.safeParse(req.body);
 
+    if(!parsedBody.success){
+        sendValidationError(res, parsedBody.error);
+        return;
+    }
+
+    const user = await prisma.user.findUnique({
+        where: {
+            username: parsedBody.data.username,
+        }
+    });
+
+    if(!user){
+        res.status(400).json({
+            success: false,
+            msg: "user doesn't exists",
+        })
+        return;
+    }
+
+    const isMatch = await bcrypt.compare(parsedBody.data.password, user?.password);
+
+    if(!isMatch){
+        res.status(400).json({
+            success: false,
+            msg: "password is not correct",
+        })
+        return;
+    }
+
+    
+    const token = jwt.sign({userId: user.id}, 
+        env.jwt_secret
+    );
+
+    res.status(200).json({
+        token,
+    })
 }
--- apps/backend/src/utils/env.ts
+++ apps/backend/src/utils/env.ts
@@ -3,4 +3,5 @@ import "dotenv/config";
 
 export const env = {
     port: (process.env.PORT ?? 3000),
+    jwt_secret: (process.env.JWT_SECRET ?? "random")
 }
