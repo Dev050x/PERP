65918acc8cefeaa67970f927d424d5a03cd7ce35
0xDev05 <divpatel1274@gmail.com>
2026-08-03 23:29:31 +0530

    refactor(engine): refactor get-depth code

--- apps/engine/src/controllers/get-depth.ts
+++ apps/engine/src/controllers/get-depth.ts
@@ -1,33 +1,32 @@
-import type { GetDepthData } from "types/publisher"
-import { OrderBookManager } from "../store/orderbook-manager"
-import { PRECISION, toString } from "../utils/conversion";
+import { OrderBookManager } from "../store/orderbook-manager";
+import { toString } from "../utils/conversion";
 
 interface Depth {
-    bids: [string, string][];
-    asks: [string, string][];
+  bids: [string, string][];
+  asks: [string, string][];
 }
 
 export function getDepth(market: string) {
-    const bestPrices = OrderBookManager.getInstance().getBestPriceOfAsset(market);
-    const depth: Depth = {
-        bids: [],
-        asks: []
-    };
-    const bids = bestPrices?.bids;
-    const asks = bestPrices?.asks;
-    if (bids) {
-        for (const [price, qty] of bids.entries()) {
-            depth.bids.push([toString(price), toString(qty)]);
-        }
+  const bestPrices = OrderBookManager.getInstance().getBestPriceOfAsset(market);
+  const depth: Depth = {
+    bids: [],
+    asks: [],
+  };
+  const bids = bestPrices?.bids;
+  const asks = bestPrices?.asks;
+  if (bids) {
+    for (const [price, qty] of bids.entries()) {
+      depth.bids.push([toString(price), toString(qty)]);
     }
-    if (asks) {
-        for (const [price, qty] of asks.entries()) {
-            depth.asks.push([toString(price), toString(qty)]);
-        }
+  }
+  if (asks) {
+    for (const [price, qty] of asks.entries()) {
+      depth.asks.push([toString(price), toString(qty)]);
     }
+  }
 
-    return {
-        depth: depth,
-        market: market,
-    };
+  return {
+    depth: depth,
+    market: market,
+  };
 }
